73954658a6cf8e06016520db1748740d827e4add
Isaac Farciert <rfisaac2296@yahoo.com>
2020-09-08 13:09:01 -0400

    add basic routes structure

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <>
+    <Router>
+
+      
+      {/* Here is the main routing mechanism */}
+      <Switch>          
+        <Route path="/announcement-boards">
+          <div>Render announcement boards component!</div>
+        </Route>
+
+        <Route path="/menu">
+          <div>Render the menu component!</div>
+        </Route>
+
+        <Route path="/events">
+          <div>Render the events section!</div>
+        </Route>
+
+        <Route path="/map">
+          <div>Render the map component!</div>
+        </Route>
+
+        <Route path="/">
+          <div>Error component</div>
+        </Route>
+      </Switch>
+    </Router>      
+    </>
   );
 }
 

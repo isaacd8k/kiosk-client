6ca4836f5efbadfccdeb39858725b7cc5c557b54
Isaac Farciert <rfisaac2296@yahoo.com>
2020-09-10 18:56:01 -0400

    create working slide change logic on parent component

--- src/components/announcement-boards/announcementboards.js
+++ src/components/announcement-boards/announcementboards.js
@@ -1,43 +1,50 @@
 import React from "react";
+import { useState } from "react";
 import styles from "./announcementboards.module.scss";
 import { motion } from "framer-motion";
+import { wrap } from "@popmotion/popcorn";
 
 // subcomponents
 import Slide from "./components/slide";
 
+
+// dummy data
+const data = [
+  { head: "heading t1", content: "lorem ipsum 1" },
+  { head: "heading t2", content: "lorem ipsum 2" },
+  { head: "heading t3", content: "lorem ipsum 3" },
+  { head: "heading t4", content: "lorem ipsum 4" },
+];
+
+// animation states (later all app anims to be abstracted into separate file)
+const animationStates = {
+  "inactive": {
+    
+  },
+  "active": {
+
+  }
+};
+
 export default function AnnouncementBoards() {
-  // slide duration (later pulled in from config)
-  const slideDuration = 30;
+  const [activeSlide, setActiveSlide] = useState(0);
 
-  // animation states (later all app anims to be abstracted into separate file)
-  const animationStates = {
-    "inactive": {
-      
-    },
-    "active": {
-
-    }
-  };
-
-  // set up timer & cleanup fn
-
-
-  // fn startSlides
-
-  // fn pauseSlides
-
-  // fn resumeSlides
-
-  // fn resetSlides
+  // paginate accepts the direction (sign) and size of jump in
+  // active slide number and sets the appropriate slide
+  const paginate = (delta) => {
+    // wrap slide increment/decrement around array size
+    const newSlideIndex = wrap(0, data.length, activeSlide + delta);
+    setActiveSlide(newSlideIndex);
+  }
 
   return (
     <>
       <div className={styles.ab_slidescontainer}>        
-        <Slide />
-        <Slide />
-        <Slide />
-        <Slide />
+        <Slide {... data[activeSlide]} />
       </div>
+
+      <button onClick={()=> { paginate(-1) }}>Previous slide</button>
+      <button onClick={()=> { paginate(1) } }>Next slide</button>
     </>
   )
 }

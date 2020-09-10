ca87df63f83bf3b54aa4d21922e0902cd959c58b
Isaac Farciert <rfisaac2296@yahoo.com>
2020-09-10 17:53:58 -0400

    set up infrastructure for a-boards layout experimenting

--- src/components/announcement-boards/announcementboards.js
+++ src/components/announcement-boards/announcementboards.js
@@ -5,16 +5,27 @@ import styles from "./announcementboards.module.scss";
 import Slide from "./components/slide";
 
 export default function AnnouncementBoards() {
+  // slide duration (later pulled in from config)
+  const slideDuration = 30;
 
-    // timer - play, pause, resume, reset
-    // slides data [title, content]
+  // set up timer & cleanup fn
+
+
+  // fn startSlides
+
+  // fn pauseSlides
+
+  // fn resumeSlides
+
+  // fn resetSlides
 
   return (
     <>
-      <div className={styles.ab_slidescontainer}>
-        
+      <div className={styles.ab_slidescontainer}>        
         <Slide />
-
+        <Slide />
+        <Slide />
+        <Slide />
       </div>
     </>
   )

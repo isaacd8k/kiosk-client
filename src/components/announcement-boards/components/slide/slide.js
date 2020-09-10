b382b198876b0f136184d95b0b77d7a69c7d4ebf
Isaac Farciert <rfisaac2296@yahoo.com>
2020-09-10 19:04:39 -0400

    refactor slide component to use dummy props data

--- src/components/announcement-boards/components/slide/slide.js
+++ src/components/announcement-boards/components/slide/slide.js
@@ -1,16 +1,16 @@
 import React from "react";
 import styles from "./slide.module.scss";
 
-export default function Slide() {
+export default function Slide({head, content}) {
   return (
     <div className={styles.ab_slide}>
       <header>
         {/* Insert SVG icon placeholder here */}
-        <span className={styles.ab_title}>(Interpolated title)</span>
+        <span className={styles.ab_title}>{head}</span>
       </header>
 
       <div className={styles.ab_content}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus dicta eveniet neque dolores eius explicabo doloremque libero delectus nisi magni. Ex delectus molestias, laborum officia facere laudantium ullam architecto sint!
+        {content}
       </div>
     </div>
   )

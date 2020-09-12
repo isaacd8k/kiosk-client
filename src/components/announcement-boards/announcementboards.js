cde0e90f4c8ab9b59cfc34d15b8c40869837efc4
Isaac Farciert <rfisaac2296@yahoo.com>
2020-09-11 20:19:52 -0400

    position the a-board containers during animation

--- src/components/announcement-boards/announcementboards.js
+++ src/components/announcement-boards/announcementboards.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import styles from "./announcementboards.module.scss";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { wrap } from "@popmotion/popcorn";
 
 // subcomponents
@@ -10,19 +10,26 @@ import Slide from "./components/slide";
 
 // dummy data
 const data = [
-  { head: "heading t1", content: "lorem ipsum 1" },
-  { head: "heading t2", content: "lorem ipsum 2" },
-  { head: "heading t3", content: "lorem ipsum 3" },
-  { head: "heading t4", content: "lorem ipsum 4" },
+  { id:0, head: "heading t1", content: "lorem ipsum 1" },
+  { id:4, head: "heading t2", content: "lorem ipsum 2" },
+  { id:7, head: "heading t3", content: "lorem ipsum 3" },
+  { id:2, head: "heading t4", content: "lorem ipsum 4" },
 ];
 
 // animation states (later all app anims to be abstracted into separate file)
 const animationStates = {
   "inactive": {
-    
+    opacity: 0,
+    transition: {
+      duration: 0.5
+    }
   },
   "active": {
-
+    opacity: 1,
+    transition: {
+      duration: 1,
+      delay: 1
+    }
   }
 };
 
@@ -30,7 +37,7 @@ export default function AnnouncementBoards() {
   const [activeSlide, setActiveSlide] = useState(0);
 
   // paginate accepts the direction (sign) and size of jump in
-  // active slide number and sets the appropriate slide
+  // active slide number and moves delta steps
   const paginate = (delta) => {
     // wrap slide increment/decrement around array size
     const newSlideIndex = wrap(0, data.length, activeSlide + delta);
@@ -39,8 +46,19 @@ export default function AnnouncementBoards() {
 
   return (
     <>
-      <div className={styles.ab_slidescontainer}>        
-        <Slide {... data[activeSlide]} />
+      <div className={styles.ab_slidescontainer}>
+        <AnimatePresence>
+          <motion.div
+            className={styles.singleslide_container}
+            key={data[activeSlide].id}
+            variants={animationStates}
+            initial="inactive"
+            animate="active"
+            exit="inactive"
+          >
+            <Slide {... data[activeSlide]} />
+          </motion.div>          
+        </AnimatePresence>
       </div>
 
       <button onClick={()=> { paginate(-1) }}>Previous slide</button>

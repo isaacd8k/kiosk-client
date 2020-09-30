d47219079a82a82446f49318e0d74e65cf6a6e1f
Isaac Farciert <rfisaac2296@yahoo.com>
2020-09-30 11:14:10 -0400

    cleanup and start fresh custom animation implementation for countdown & animation

--- src/components/announcement-boards/announcementboards.js
+++ src/components/announcement-boards/announcementboards.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback, useMemo, useRef } from "react";
 import styles from "./announcementboards.module.scss";
 import { motion, AnimatePresence, useAnimation } from "framer-motion";
 import { wrap } from "@popmotion/popcorn";
 
 // custom hooks
-import { useInterval } from "./hooks";
+// import { useInterval } from "./hooks";
 
 // subcomponents
 import Slide from "./components/slide";
@@ -14,8 +14,8 @@ import Slide from "./components/slide";
 // dummy data
 const data = [
   { id:0, head: "heading t1", content: "lorem ipsum 1" },
-  { id:4, head: "heading t2", content: "lorem ipsum 2" },
-  { id:7, head: "heading t3", content: "lorem ipsum 3" },
+  { id:4, head: "heading t2", content: "lorem ipsum 2", duration: 8 },
+  { id:7, head: "heading t3", content: "lorem ipsum 3", duration: 20 },
   { id:2, head: "heading t4", content: "lorem ipsum 4" },
 ];
 
@@ -40,95 +40,61 @@ const animationStates = {
 
 
 export default function AnnouncementBoards() {
-  const [activeSlide, setActiveSlide] = useState(0);
-  const [slideTimerCount, setSlideTimerCount] = useState(100);
-  const [delay, setDelay] = useState(null);
-  const [isRunning, setIsRunning] = useState(true);
+  const [slides, setSlides] = useState(data);
+  const [activeSlideIndex, setActiveSlideIndex] = useState(undefined);
   
-  const [slideDelay, setSlideDelay] = useState(30);
 
-  // animation orchestration
-  const controls = useAnimation();
+  /*
+    ***** bug watch *****
+    -- Note that if the activeSlideIndex is 0, JS coerces the type into
+    -- numeric when used with numeric operators such as > and >=.
+    -- Therefore, we have set up the state to start out as undefined
+    -- so that these numeric operators can be used as numeric 
+    -- comparators below
+  */
 
-
-  // set up default tick delay on initial render
-  useEffect(() => setDelay(100), []);
-
-
-  // set up the tick function at configured delay (countdown) - null pauses the countdown
-  useInterval(() => {
-    setSlideTimerCount((current) => current - 1);
-  }, isRunning ? delay : null);
-
-
-  // react when counter reaches zero, move to next page and reset
   useEffect(() => {
-    if (slideTimerCount === 0) {
-      paginate(1);
-      setSlideTimerCount(100);
-    }
-  }, [slideTimerCount]);
+    setActiveSlideIndex(0);
+  }, [slides])
 
-
+  
   // moves the slider `delta` slides forward
   const paginate = (delta) => {    
-    setActiveSlide((currentSlide) => (
+    setActiveSlideIndex((currentSlide) => (
       // wrap slide increment/decrement around array size
-      wrap(0, data.length, currentSlide + delta)
+      wrap(0, slides.length, currentSlide + delta)
     ));
   };
 
-  const playSlides = () => {
-    controls.start("fillStroke");
-    setIsRunning(true);
-  }
 
-  const pauseSlides = () => {
-    controls.stop();
-    setIsRunning(false);
-  }
-
-  const variants = {
-    blankStroke: {
-      pathLength: 1,
-      pathOffset: 0
-    },
-    fillStroke: (duration) => ({
-      pathLength: 1,
-      pathOffset: 1,
-      transition: { duration, ease:"linear" }
-    })
-  }
-  
 
   return (
     <>
       <div className={styles.ab_slidescontainer}>
-        <AnimatePresence>
-          <motion.div
-            className={styles.singleslide_container}
-            key={data[activeSlide].id}
-            variants={animationStates}
-            initial="inactive"
-            animate="active"
-            exit="inactive"
-          >
-            <Slide {... data[activeSlide]} />
-          </motion.div>          
-        </AnimatePresence>
+        { (activeSlideIndex >= 0) &&
+          <AnimatePresence>
+            <motion.div
+              className={styles.singleslide_container}
+              key={data[activeSlideIndex].id}
+              variants={animationStates}
+              initial="inactive"
+              animate="active"
+              exit="inactive"
+            >
+              <Slide {... data[activeSlideIndex]} />
+            </motion.div>
+          </AnimatePresence>
+        }
       </div>
 
       <button onClick={()=> { paginate(-1) }}>Previous slide</button>
       <button onClick={()=> { paginate(1) } }>Next slide</button>
-      <button onClick={()=> pauseSlides() }>Pause slides</button>
-      <button onClick={()=> playSlides() }>Play slides</button>
-
-      <div>Countdown: {slideTimerCount}</div>
+      
 
 
 
       <div className={styles.svgContainer}>
-        <motion.svg
+        <svg
           viewBox="0 0 850 650" 
           className={styles.svg} 
           preserveAspectRatio="none"
@@ -142,19 +108,16 @@ export default function AnnouncementBoards() {
             strokeLinejoin="round"
             strokeLinecap="round"
           />
-          <motion.path
+          <path
             d="M20 20 H788 V588 H20 z"
             fill="transparent"
             stroke="#f3f3f3"
             strokeWidth="6"
             strokeLinejoin="round"
             strokeLinecap="square"
-            variants={variants}
             initial="blankStroke"
-            animate={controls}
-            custom={slideDelay}
           />
-        </motion.svg>
+        </svg>
       </div>
     </>
   )
